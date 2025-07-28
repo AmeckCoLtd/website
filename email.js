4d80bc442056e570b278cc3bd861b0e894e0416b
AmeckCoLtd <AmeckLtdTanzania@hotmail.com>
2025-07-28 11:58:20 +0300

    Add files via upload

---
+++ email.js
@@ -0,0 +1,4 @@
+// Add your EmailJS IDs here
+const serviceID = 'YOUR_SERVICE_ID';
+const templateID = 'YOUR_TEMPLATE_ID';
+const userID = 'YOUR_USER_ID';
---
+++ invoice.js
@@ -0,0 +1,1 @@
+// JS logic for creating, printing, and saving invoice
